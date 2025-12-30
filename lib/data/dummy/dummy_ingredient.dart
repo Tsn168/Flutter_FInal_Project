8d92f8ba67801254b21612e028f6f343ca0b6533
somnang tep <163022251+Tsn168@users.noreply.github.com>
2025-12-30 15:52:06 +0700

    anh hort hx

---
+++ lib/data/dummy/dummy_ingredient.dart
@@ -0,0 +1,8 @@
+// dummy/dummy_ingredient.dart
+import '../models/ingredient.dart';
+
+const flour = Ingredient(id: 'i1', name: 'Flour', quantity: '2 cups');
+const eggs = Ingredient(id: 'i2', name: 'Eggs', quantity: '6 pcs');
+const milk = Ingredient(id: 'i3', name: 'Milk', quantity: '500 ml');
+const sugar = Ingredient(id: 'i4', name: 'Sugar', quantity: '1 cup');
+const butter = Ingredient(id: 'i5', name: 'Butter', quantity: '100 g');
---
+++ lib/data/dummy/dummy_recipe.dart
@@ -0,0 +1,20 @@
+import 'dummy_ingredient.dart';
+
+final pancakeRecipe = Recipe(
+  id: 'r1',
+  title: 'Pancakes',
+  ingredients: [
+    flour,
+    eggs,
+    milk,
+    sugar,
+  ],
+);
+
+final scrambledEggsRecipe = Recipe(
+  id: 'r2',
+  title: 'Scrambled Eggs',
+  ingredients: [eggs, milk, butter],
+);
+
+final allRecipes = [pancakeRecipe, scrambledEggsRecipe];
