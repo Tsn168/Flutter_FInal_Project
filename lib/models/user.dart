728c6a6bda76e616fafb6d85378cd5220687c4f9
somnang tep <163022251+Tsn168@users.noreply.github.com>
2026-01-12 11:48:36 +0700

    remove the create

--- lib/models/user.dart
+++ lib/models/user.dart
@@ -3,14 +3,12 @@ class User {
   final String username;
   final String email;
   final String passwordHash;
-  final int createdAt;
 
   User({
     required this.id,
     required this.username,
     required this.email,
     required this.passwordHash,
-    required this.createdAt,
   });
 
   // Convert User to Map for database INSERT
@@ -20,7 +18,6 @@ class User {
       'email': email,
       'username': username,
       'password_hash': passwordHash,
-      'created_at': createdAt,
     };
   }
 
@@ -31,7 +28,6 @@ class User {
       email: map['email'] as String,
       username: map['username'] as String,
       passwordHash: map['password_hash'] as String,
-      createdAt: map['created_at'] as int,
     );
   }
 }
